5fd3b8c3cf199a3897c58f1325cc8606fbf27fcb
endless0922 <leonpwlwsk0713@gmail.com>
2020-05-09 15:55:21 +0300

    Fix: parametrized small talk

--- api/dialogflow_api.py
+++ api/dialogflow_api.py
@@ -2,17 +2,16 @@ import json
 
 import apiai
 
-import message_generator
 import security
 
 
 def call_small_talk(message):
     try:
         request = apiai.ApiAI(security.DIALOG_FLOW_TOKEN).text_request()
-        request.lang = 'en'
-        request.session_id = 'BayaBot'
+        request.lang = security.DIALOG_FLOW_LANGUAGE
+        request.session_id = security.DIALOG_FLOW_NAME
         request.query = message
         response = json.loads(request.getresponse().read().decode('utf-8'))
-        return response['result']['fulfillment']['speech'] + " " + message_generator.Emoji.smile
+        return response['result']['fulfillment']['speech']
     except Exception as e:
         raise Exception('Can not get information' + str(e))
--- security.py
+++ security.py
@@ -5,5 +5,6 @@ config.read('bot.ini')
 
 TOKEN = config['Telegram']['token']
 PROXY = config['Telegram']['proxy']
+DIALOG_FLOW_NAME = config['DialogFlow']['name']
+DIALOG_FLOW_LANGUAGE = config['DialogFlow']['language']
 DIALOG_FLOW_TOKEN = config['DialogFlow']['token']
-
